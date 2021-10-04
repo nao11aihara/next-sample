9dc65cbe7b98421501a1f26a68e5958e4f1d25c5
nao11aihara <nao11aihara@gmail.com>
2021-10-04 23:48:51 +0900

    add: コメント追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from "next/app";
 
+/**
+ * App
+ * 全てのpagesコンポーネントで呼び出される最上位コンポーネント
+ */
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
