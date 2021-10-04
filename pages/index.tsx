78253af43e73265a676176f9d18d3278b508cc14
nao11aihara <nao11aihara@gmail.com>
2021-10-04 23:49:21 +0900

    add: Headerコンポーネント追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { Header } from "./../components/Header";
 
+/**
+ * トップ画面
+ */
 const TopPage: NextPage = () => {
   return (
     <>
       <Head>
         <title>トップ</title>
       </Head>
+      <Header></Header>
       <div>トップ</div>
       <Link href="/new">
         <button>新規作成画面へ</button>
--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { Header } from "./../../components/Header";
 
+/**
+ * 記事新規作成
+ */
 const ArticleNewPage: NextPage = () => {
   return (
     <>
       <Head>
         <title>記事新規作成</title>
       </Head>
+      <Header></Header>
       <div>記事新規作成</div>
       <Link href="/">
         <button>トップへ</button>
