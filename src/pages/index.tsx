7ce049a2cc5e088cc07e75f48261078238187710
nao11aihara <nao11aihara@gmail.com>
2021-10-16 23:55:30 +0900

    update: pages、componentsディレクトリをsrcディレクトリ配下に移動

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { Header } from "./../components/Header";
+import { Header } from "../components/Header";
 
 /**
  * トップ画面
